b7d14efeb9db6879a00cd00078e70c521778cccd
Eloi BERLINGER <eloi.berling@protonmail.com>
2025-10-20 19:28:51 +0200

    Init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { DishwasherDashboard } from './components/DishwasherDashboard';
+
+function App(): React.ReactElement {
+  return <DishwasherDashboard />;
+}
+
+export default App;
+
+
